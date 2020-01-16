d2d869cfe3b99e611df410126d60e3a7ab8db5d9
Jirashy <jirachai@madvrstdio.com>
2020-01-16 23:08:13 +0700

    - Add Profile

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,16 +4,40 @@ import ReactDOM from 'react-dom';
 class About extends React.Component {
     render() {
         return (
-            <div id="about-container" className="content-container container text-center">
+            <div id="about-container" className="container text-center mt-5">
                 <h1>About</h1>
-                <div className="about-content-container">
-                    <p>Jirachai Kongjinda</p>
-                    <p>Jirachai Kongjinda</p>
-                    <p>Jirachai Kongjinda</p>
-                    <p>Jirachai Kongjinda</p>
-                    <p>Jirachai Kongjinda</p>
-                    <p>Jirachai Kongjinda</p>
-                    </div>  
+                <div id="about-content-container" className="row mt-5">
+                     <div className="col">
+                        <div className="row h-100">
+                            <div className="col-6">
+                                <div className="row align-items-center h-100">
+                                    <div className="col">
+                                        <p>Image</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="row h-50 mt-4">
+                                    <div className="col">
+                                            <h1 className="about-padding text-left">Hello, my name is Grape.</h1>
+                                            <p className="about-profile about-padding text-left mt-2">
+                                                I am a Developer who like to make and play games, 
+                                                Outside of work I like to play the guitar or find something new to learn. 
+                                                Currently I am working as a Game Developer as MAD Virtual Reality Studio. 
+                                                I'd love to get to know you, so don't hesitate to get in touch.
+                                            </p>                                     
+                                    </div>
+                                </div>
+                                <div className="row align-items-center h-50">
+                                    <div className="col">
+                                            <p>Skills.
+                                            </p>                                     
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>  
             </div>
         )
     }        
